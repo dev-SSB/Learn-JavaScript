90a953174702292482d227239c10a942c94ac0ed
shiv bodke <shivbodke03@gmail.com>
2024-10-17 19:27:50 +0530

    16/10/24 done

--- 01-Basics/06-String.js
+++ 01-Basics/06-String.js
@@ -1,13 +1,26 @@
-// 2 ways of string declaration
+/*  2 ways of string declaration 
+    1) String Literal : Create Primitive String And Directly Stored in Memory
+    2) String Constructor : Create an Object That contain Primitive String Value and additional 
+        Properties & Methods. 
+*/
 
 let str = "SSB";
 let strNew = new String("Ayu");
 
 // console.log(str);
+// -- SSB --
+
 // console.log(strNew);
+// -- [String: 'Ayu'] --
+
+// console.log(typeof strNew);
+// -- object --
 
 // console.log(strNew[0]);
+// -- A --
+
 // console.log(strNew.__proto__);
+// -- {} --
 
 
 
@@ -16,16 +29,21 @@ let strNew = new String("Ayu");
 // New way of Use
 const username = "shiv";
 const age = 15;
+
 // console.log(`my name is ${username} & age is ${age}`);
+// my name is shiv & age is 15
 
 // console.log( username.length );
+// 4
 
 // console.log( username.toUpperCase() );
+// SHIV
 
 // console.log( username.charAt(2) );
+// i
 
 // console.log( username.indexOf('v') );
-
+// 3
 
 
 
@@ -37,11 +55,7 @@ const age = 15;
 // trim --> starting and ending white space are remove.
 let name2 = "          pk        ";
 // console.log( name2.trim() );
-
-
-
-
-
+// "pk"
 
 
 
@@ -52,21 +66,31 @@ let name1 = "shiv-ssb-dev-euu";
 // take -ve value Not Allowed
 // substring(start,end)
 // console.log( name1.substring(0,6) );
+// shiv-s
 
+/*
+take -ve & +ve value Allowed
+It returns a new string and does not alter the original string.
+Negative indices can be used to count from the end of the string
 
-// take -ve value Allowed
+let str = "Hello, World!";
+let sliced = str.slice(-6); // "World!"
+
+*/
 // console.log( name1.slice(0,8) );
+// shiv-ssb
 
 
 // replace()
 // only first match '-' will replace to '@'
 const newName2 = name1.replace('-','@')
 // console.log( newName2 );
+// shiv@ssb-dev-euu
 
 
 // include()
-// console.log( name1.includes('dev') );
-
+console.log( name1.includes('dev') );
+// true
 
 
 
@@ -76,4 +100,4 @@ const newName2 = name1.replace('-','@')
 // split
 // separate and make array --> [ 'SSB', 'Shiv', 'PK', 'AYU' ]
 const temp = "SSB-Shiv-PK-AYU";
-// console.log( temp.split('-') );
+console.log( temp.split('-') );
--- 01-Basics/08-Date-Time.js
+++ 01-Basics/08-Date-Time.js
@@ -3,28 +3,32 @@
 let myDate = new Date();
 
 // console.log(myDate);
+// 2024-10-17T13:43:47.790
 
 // console.log(myDate.toString());
+// Thu Oct 17 2024 19:15:00 GMT+0530 (India Standard Time)
 
 // console.log(myDate.toDateString());
+// Thu Oct 17 2024
 
 // console.log(myDate.toLocaleString());
-
+// 17/10/2024, 7:18:05 pm
 
 
 // 
 let myCreatedDate1 = new Date(2023,0,23);
 // console.log( myCreatedDate1.toDateString() );
-
+// Mon Jan 23 2023
 
 // 
 let myCreatedDate2 = new Date(2023,0,23,5,3);
 // console.log( myCreatedDate2.toLocaleString() );
-
+// 23/1/2023, 5:03:00 am
 
 // YY MM DD
 let myCreatedDate3 = new Date("2023-01-14");
 // console.log( myCreatedDate3.toLocaleString() );
+// 14/1/2023, 5:30:00 am
 
 
 
@@ -40,16 +44,21 @@ let myCreatedDate3 = new Date("2023-01-14");
 // Time in Milli Second
 let myTimeStamp = Date.now();
 // console.log( myTimeStamp );
+// 1729173391915
 
 // Time in Milli Second to Second
 // console.log( Math.floor( Date.now()/1000 ) );
-
+// 1729173407
 
 
 // Month Start from 0 
 let myCreatedDate = new Date();
+
 // console.log( myCreatedDate.getMonth() );
+// 9
+
 // console.log( myCreatedDate.getFullYear() );
+// 24
 
 
 // Customize format of toLocaleString()
