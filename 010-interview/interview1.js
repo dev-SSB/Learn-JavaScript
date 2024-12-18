cbff0ffabcc42bd2e82d244a6217c02b9fccaa43
shiv bodke <shivbodke03@gmail.com>
2024-12-18 19:41:46 +0530

    done

---
+++ 010-interview/interview1.js
@@ -0,0 +1,107 @@
+// function abc(){
+//     console.log(abc.xyz);
+// }
+
+// abc()
+// abc.xyz=200
+// abc.xyz=400
+// abc()
+
+
+
+// What is Event propogation
+
+
+
+// valid but we get empty items from 4 to 99 position
+// const num =[1,2,3,4];
+// num[100]=500;
+// console.log(num);
+
+
+
+// String
+// console.log(typeof typeof 100)
+
+
+// [ 'S', 'S', 'B' ]
+// const arr = [..."SSB"]
+// console.log(arr);
+
+
+
+// parseInt
+// console.log(parseInt('10+2'));
+// console.log(parseInt('7fm'));
+// console.log(parseInt('m7f'));
+// console.log(isNaN('SSB'))
+
+
+
+// console.log([1,2].map(num =>{
+//     if(num>0)
+//         return;
+//     return num*2;
+// }));
+
+
+
+// function abc(){
+//     return ;
+// }
+// console.log(abc());
+
+
+
+
+// {
+//     function abc() {
+//         console.log("praveen");
+//     }
+// }
+// abc()
+
+
+
+// 'use strict';
+// {
+//     function abc() {
+//         console.log("praveen");
+//     }
+// }
+// abc()
+
+
+
+
+// function abc(a,b){
+//     'use strict';
+//     a=100
+//     b=200
+//     return arguments[0]+args[1]
+//     console.log(a+b);
+// }
+// console.log(abc(300,400));
+
+
+
+// function abc(a,b){
+//     'use strict';
+//     a=100
+//     b=200
+//     return arguments[0]+arguments[1]
+//     console.log(a+b);
+// }
+// console.log(abc(300,400));
+
+
+
+// slice method
+// splice method
+// find method
+
+
+
+
+
+
